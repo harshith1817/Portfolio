4d23e0b6b3671d8946b417dcbf5d1526b37143d1
HARSHITH CHINTAKINDI <chintakindibhargav2016@gmail.com>
2024-06-01 00:29:39 +0530

    Resume Image Updated

--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -1,36 +1,72 @@
-import React, { useState } from 'react';
-import { BsDownload } from "react-icons/bs";
-import pdf from "./Resume.pdf";
-import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+import './index.css';
+import styled from 'styled-components';
+import { IoMdDownload } from "react-icons/io";
+import pdfFile from './Resume.pdf';
+import Image from './Resume.jpg'
 
+const ResumePage = styled.section`
+  width: 100%;
+`;
 
-const Resume = () => {
-  const[wid, setwid]=useState(window.innerWidth);
+const ResumeDiv = styled.div`
+  height: 52rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 6rem;
+`;
 
-  const handleResize=()=>{
-    setwid(window.innerWidth);
+const ResumeImg = styled.div`
+  width: 43%;
+  height: 47rem;
+  background-size: cover;
+  background-position: center;
+  background-color:white;
+`;
+
+const DownloadContainer = styled.div`
+  height: 4rem;
+  display: flex;
+  align-items: start;
+  justify-content: center;
+`;
+
+const Download = styled.a`
+  background-color: #8a4ca8;
+  height: 2.5rem;
+  width: 13rem;
+  border-radius: 0.35rem;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.1rem;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  text-decoration: none;
+
+  &:hover {
+    opacity: 0.8;
   }
+`;
 
-  window.addEventListener("load", handleResize);
-  window.addEventListener("resize", handleResize);
+function Resume() {
 
   return (
-    <div className='ResumePage'>
-      <Document file={pdf} className="resumeview">
-          <Page pageNumber= {1} scale={wid<700 ? ( wid>475? 0.7: 0.5): 1}/>
-      </Document>
-
-      <a href={pdf} target='_blank' download="Devansh's Resume">
-        <button className='downloadCV' type='button'>
-          <h3><BsDownload/>&nbsp; Download CV</h3>
-        </button>
-      </a>
-
-    </div>
-  )
+    <ResumePage>
+      <ResumeDiv>
+          <ResumeImg>
+          <img src={Image} className="ResumeImage"/>
+          </ResumeImg>
+      </ResumeDiv>
+      <DownloadContainer>
+        <Download href={pdfFile} download>
+          <IoMdDownload /> Download Resume
+        </Download>
+      </DownloadContainer>
+    </ResumePage>
+  );
 }
 
-export default Resume
+export default Resume;
